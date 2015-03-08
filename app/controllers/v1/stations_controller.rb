b20d055f8c91d062297f4a6dcabf4cf5e22ad74a
Ismael Mendonca <irmt06@gmail.com>
2015-03-08 14:34:38 +0100

    Added station as a nested resource of assembly line.

--- app/controllers/v1/stations_controller.rb
+++ app/controllers/v1/stations_controller.rb
@@ -2,8 +2,10 @@ class V1::StationsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    station = Station.create station_params
-    render json: station
+    assembly_line = AssemblyLine.find params[:assembly_line_id]
+    station = assembly_line.stations.create station_params[:station]
+    
+    render json: station 
   end
 
   def destroy
@@ -13,9 +15,8 @@ class V1::StationsController < ApplicationController
     render json: station
   end
 
-
   def station_params
-    params.permit(:name, :estimated_time, "assembly_line_id")
+    params.permit(:assembly_line_id, :station => [:name, :estimation_time])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    resources :assembly_lines
-    resources :stations
+    resources :assembly_lines do
+      resources :stations
+    end
   end
 end
