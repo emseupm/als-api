3cc645027a8d5d73c8336d4f24b8fc4b7f55eec2
Daniel Cestari <dcestari@gmail.com>
2015-03-09 10:10:41 +0100

    Make test specs pass

--- spec/controllers/assembly_line_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe AssemblyLineController, :type => :controller do
-
-end
--- spec/controllers/station_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe StationController, :type => :controller do
-
-end
