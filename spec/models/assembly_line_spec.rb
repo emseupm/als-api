37531233370483e67b288bf6ca5ebc6f3b59dcdf
Daniel Cestari <dcestari@gmail.com>
2015-03-09 11:03:47 +0100

    Add specs for AssemblyLine model

--- spec/models/assembly_line_spec.rb
+++ spec/models/assembly_line_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe AssemblyLine, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe AssemblyLine, type: :model do
+
+  it '.name' do
+    name = 'Ford Focus'
+    assembly_line = AssemblyLine.create name: name
+    expect(assembly_line.name).to eq(name)
+  end
+
+  it '.stations' do
+    assembly_line = AssemblyLine.create name: 'Ford Focus'
+    station = Station.create name: 'Engine Setup', assembly_line_id: assembly_line.id
+    expect(assembly_line.stations.to_a).to eq([ station ])
+  end
+
+  context 'accepts nested attributes for stations' do
+    let(:station_name) { 'Engine Setup' }
+    let(:stations_attributes) { [ { name: station_name } ] }
+    let(:assembly_line) { AssemblyLine.create name: 'Ford Focus', stations_attributes: stations_attributes }
+
+    it do
+      expect(assembly_line.stations.count).to be(1)
+    end
+
+    it do
+      expect(assembly_line.stations.first.name).to eq(station_name)
+    end
+  end
+
 end
